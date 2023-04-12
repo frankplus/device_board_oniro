53fd41c0f439150771c33819c9987bcacc8a7bb0
Diemit <598757652@qq.com>
2023-04-12 18:31:13 +0800

    添加蓝牙相关库

--- rpi4/cfg/init.modprobe.sh
+++ rpi4/cfg/init.modprobe.sh
@@ -12,5 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#bluetooth
+modprobe hci_uart
+modprobe bnep
+modprobe rfcomm
 modprobe bluetooth
+
+#wifi
 modprobe brcmfmac
